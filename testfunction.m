518f4459bebc67f38cb97669773c993e0f1196cc
popotracy <yu-tzu.wu2@mail.mcgill.ca>
2023-10-24 12:23:35 -0400

    update

--- testfunction.m
+++ testfunction.m
@@ -14,3 +14,20 @@ function [arg1, arg2]=testfunction(var)
     
     
 end
+
+%% 
+Conditions={'cuedPref' 'cuedFast'};
+cfg.dataset   = 'P2_01_cuedPref_sync.set';  % Run the dataset in the syncfolder
+cfg.TaskName        =  cfg.dataset;
+strstart=strfind(cfg.dataset, Conditions{1});
+strend=strstart+length(Conditions{1})-1;
+cfg.dataset(strstart:strend);
+
+%%
+path='/Users/tracy/Desktop/UdeM/BIDS/P2_syncdata (18-Oct-2023)'
+EEGfilenames={dir([path '*.set']).name};
+[~, Conditionname, ~]=fileparts(EEGfilenames);
+Conditionname=cellstr(Conditionname)
+
+dir('*.set').name
+dir([Rawdatapath(k).EEG '*.vhdr'])
